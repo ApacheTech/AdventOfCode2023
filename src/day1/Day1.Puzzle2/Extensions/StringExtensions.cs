1baa91f453e08ba627a3d282fa68a6d819bac0eb
ApacheTech Solutions <p_matthews@live.co.uk>
2023-12-03 22:37:54 +0000

    feat: day 1 puzzle 2

---
+++ src/day1/Day1.Puzzle2/Extensions/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace Day1.Puzzle2.Extensions;
+
+public static class StringExtensions
+{
+    public static string ConvertWordsToDigits(this string input)
+        => input
+            .ConvertWordsToDigits(Overlaps)
+            .ConvertWordsToDigits(NumberWords);
+
+    public static IList<char> StripLetters(this string input) 
+        => string.Concat(input.Where(char.IsDigit)).ToList();
+
+    public static int ConcatenateDigits(this IList<char> input) 
+        => input.Count == 0 ? 0 : int.Parse($"{input.First()}{input.Last()}");
+    private static string ConvertWordsToDigits(this string input, Dictionary<string, string> substitutes)
+        => substitutes.Aggregate(input, (current, substitute) => current.Replace(substitute.Key, substitute.Value));
+
+    private static readonly Dictionary<string, string> Overlaps = new()
+    {
+        {"oneight", "18"},
+        {"twone", "21"},
+        {"threeight", "38"},
+        {"fiveight", "58"},
+        {"sevenine", "79"},
+        {"eightwo", "82"},
+        {"eighthree", "83"},
+        {"nineight", "98"}
+    };
+
+    private static readonly Dictionary<string, string> NumberWords = new()
+    {
+        {"one", "1"},
+        {"two", "2"},
+        {"three", "3"},
+        {"four", "4"},
+        {"five", "5"},
+        {"six", "6"},
+        {"seven", "7"},
+        {"eight", "8"},
+        {"nine", "9"},
+    };
+}
---
+++ src/day1/Day1.Puzzle2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day1.Puzzle2.Extensions;
+var lines = await File.ReadAllLinesAsync("input.txt");
+Console.WriteLine(lines
+    .Select(p => p.ConvertWordsToDigits())
+    .Select(p => p.StripLetters())
+    .Select(p => p.ConcatenateDigits())
+    .Sum());
