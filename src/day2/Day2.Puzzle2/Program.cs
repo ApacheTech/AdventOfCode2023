b488d1be0e2f76d6819956e0bee5c577c9668097
ApacheTech Solutions <p_matthews@live.co.uk>
2023-12-03 23:31:40 +0000

    feat: day 2 puzzle 2

---
+++ src/day2/Day2.Puzzle2/Program.cs
@@ -0,0 +1,6 @@
+﻿using Day2.Puzzle1;
+var lines = await File.ReadAllLinesAsync("input.txt");
+Console.WriteLine(lines
+    .Select(p => new Game(p))
+    .Select(p => p.RedCubes.Max() * p.GreenCubes.Max() * p.BlueCubes.Max())
+    .Sum());
