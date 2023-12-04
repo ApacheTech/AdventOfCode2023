9ce5f54c94fd014b55a94d727736f335da16c18f
ApacheTech Solutions <p_matthews@live.co.uk>
2023-12-04 19:04:13 +0000

    feat: day 4 puzzle 2

---
+++ src/day4/Day4.Puzzle2/Card.cs
@@ -0,0 +1,28 @@
+﻿namespace Day4.Puzzle2;
+
+public record Card
+{
+    public Card(int cardNumber, string line)
+    {
+        CardNumber = cardNumber;
+
+        var data = line.Split(':')[1];
+        var sets = data.Split("|");
+
+        var targetNumbers = sets[0]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(p => int.Parse(p.Trim()))
+            .ToList();
+
+        var cardNumbers = sets[1]
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(p => int.Parse(p.Trim()))
+            .ToList();
+
+        Matches = cardNumbers.Intersect(targetNumbers).Count();
+    }
+
+    public int CardNumber { get; init; }
+
+    public int Matches { get; }
+}
---
+++ src/day4/Day4.Puzzle2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Day4.Puzzle2;
+
+var input = await File.ReadAllLinesAsync("input.txt");
+var cards = Enumerable.Range(0, input.Length).ToDictionary(p => p, _ => 0);
+
+for (var i = 0; i < input.Length; i++)
+{
+    cards[i]++;
+
+    for (var j = 0; j < cards[i]; j++)
+    {
+        var card = new Card(i, input[i]);
+        if (card.Matches > 0)
+        {
+            for (var k = 1; k <= card.Matches; k++)
+            {
+                var l = i + k;
+                if (l >= input.Length) continue;
+                cards[l]++;
+            }
+        }
+    }
+
+    Console.WriteLine($"ID: {i} | Cards: {cards[i]}");
+}
+
+Console.WriteLine($"Total Cards: {cards.Sum(p => p.Value)}");
